30bb2c04582857dd3e753474f2f9e109d02586d0
Bernardo Batistelli <bebatistelli254@gmail.com>
2023-10-16 16:55:12 -0300

    added getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,72 @@
-import { styled } from '../styles'
+import Image from 'next/image'
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-})
+import { useKeenSlider } from 'keen-slider/react'
 
-export default function Home() {
-  return <Button>Enviar</Button>
+import { HomeContainer, Product } from '../styles/pages/home'
+
+import 'keen-slider/keen-slider.min.css'
+import { stripe } from '../lib/stripe'
+import { GetStaticProps } from 'next'
+import Stripe from 'stripe'
+
+interface HomeProps {
+  products: {
+    id: string
+    name: string
+    imageUrl: string
+    price: number
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
+
+  return (
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      {products.map((product) => {
+        return (
+          <Product key={product.id} className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={480} alt="" />
+
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        )
+      })}
+    </HomeContainer>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price'],
+  })
+
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      }).format(price.unit_amount! / 100),
+    }
+  })
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
+  }
 }
